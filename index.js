2788a7c1ac3d0789d5ba9c3872c0d2c309a6bd8a
nikhilsmkumar <nikhilsmkumar007@gmail.com>
2018-08-06 00:29:14 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(()=>{
+	$(".main").click(()=>{
+        $(".modal").css("display","flex");
+	});
+	$(".clear").click(()=>{
+		$(".modal").css("display","none");
+	});
+	$(".button1").click(()=>{
+		$(".modal").css("display","none");
+	});	
+
+});
